3891a6024d8d46edfab8d497e24855c7be416b8f
Samuel Keim <samuel.keim@web.de>
2021-06-14 14:34:46 +0200

    Javascript hinzugefuegt

---
+++ src/main/resources/public/main.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+let button1=document.getElementById("set1p1plus")
+console.log(button1)
+button1.addEventListener("click", (event) => {
+    console.log(event);
+    console.log("test");
+})
