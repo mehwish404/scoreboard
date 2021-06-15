b54ec5dfe316774e50d0260e0adc2adf7295a59e
Samuel Keim <samuel.keim@web.de>
2021-06-15 10:29:34 +0200

    Zwischenstand Javascript Requests

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -1,32 +1,45 @@
 console.log("Hello World")
 let name1 = document.getElementById("name1");
 let name2 = document.getElementById("name2");
-test(); 
+test();
 let buttons = document.getElementsByTagName("button");
-for(let i = 0; i<buttons.length; i++) {
+console.log(buttons);
+for (let i = 0; i < buttons.length; i++) {
     let button = buttons[i];
     let player = 1;
-    button.addEventListener("click",(event) =>{
-        console.log(button.parentElement.parentElement.className);
-        let parent = button.parentElement.parentElement;
+    button.addEventListener("click", (event) => {
+        // console.log(button.parentElement.parentElement.className);
+        let parent = button.parentElement.parentElement.parentElement;
         let score1 = parent.querySelector(".score1");
         let score2 = parent.querySelector(".score2");
+
+        console.log(score1);
+        console.log(score2);
+
         if (parent.className.includes("left")) {
             player = 1;
         }
         else if (parent.className.includes("right")) {
             player = 2;
         }
-        if (button.id === "minus"){
+
+        console.log(button.classList);
+
+        if (button.classList.contains("minus")) {
             minusscore(player, score1, score2)
-        } else if (button.id === "plus") {
+        } 
+        
+        if (button.classList.contains("plus")) {
             addscore(player, score1, score2)
         }
     })
 }
-fetch("http://localhost:8080/match/1")
-    .then(function(response) {return response.json();})
-    .then(function(data) {
+
+// Initiale Abfrage von Match 1 
+function createMatch(player1, player2, courtNumber, score1, score2) {
+    fetch("http://localhost:8080/match/1")
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
         console.log(data)
         let match = data.id;
         score1.innerHTML = data.matchsets[0].score1;
@@ -34,6 +47,9 @@ fetch("http://localhost:8080/match/1")
         name1.innerHTML = data.player1;
         name2.innerHTML = data.player2;
     })
+}
+ 
+createMatch();
 
 // document.getElementById("score1plus").addEventListener("click",(event) =>{
 //     addscore(1, score1, score2);
@@ -49,38 +65,49 @@ fetch("http://localhost:8080/match/1")
 //     minusscore(2);
 // })
 
-function addscore(player, score1htmlelement, score2htmlelement){
-    value=0;
-    fetch("http://localhost:8080/match/1/1/increase/"+player, {
-        method:"POST"
+function addscore(player, score1htmlelement, score2htmlelement) {
+    value = 0;
+    fetch("http://localhost:8080/match/1/1/increase/" + player, {
+        method: "POST"
     })
-        .then(function(response) {return response.json();})
-        .then(function(data) {
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
             score1htmlelement.innerHTML = data.score1;
             score2htmlelement.innerHTML = data.score2;
         })
 }
-function minusscore(player, score1, score2){
-    value=0;
-    fetch("http://localhost:8080/match/1/1/decrease/"+player, {
-        method:"POST"
+
+
+function minusscore(player, scoreEle1, scoreEle2) {
+    value = 0;
+    fetch("http://localhost:8080/match/1/1/decrease/" + player, {
+        method: "POST"
     })
-        .then(function(response) {return response.json();})
-        .then(function(data) {
-            
-                score1.innerHTML = data.score1
-           
-                score2.innerHTML = data.score2
-            
+        .then(function (response) {
+            console.log("response: ", response);
+            const responseJson = response.json();
+            console.log("responseJson: ", responseJson);
+            return responseJson;
+        })
+        .then(function (data) {
+            console.log(scoreEle1, scoreEle2)
+            console.log("data: ", data);
+            if (data) {
+
+                scoreEle1.innerHTML = data.score1
+
+                scoreEle2.innerHTML = data.score2
+            }
+
         })
 }
-function test(){
-    value=0;
+function test() {
+    value = 0;
     fetch("http://localhost:8080/match/1/addSet", {
-        method:"POST"
+        method: "POST"
     })
-        .then(function(response) {return response.json();})
-        .then(function(data) {
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
             console.log(data)
         })
 }
